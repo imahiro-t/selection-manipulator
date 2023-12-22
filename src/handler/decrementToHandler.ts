aa45c6585aebc00dc4dc93feb32ab0e1bbfdff6b
Takashi Imahiro <imahiro@erin.jp>
2023-12-23 08:58:54 +0900

    ✨ add tools for increment from and decrement to

---
+++ src/handler/decrementToHandler.ts
@@ -0,0 +1,28 @@
+import {
+  TextEditor,
+  window,
+} from 'vscode';
+
+export const decrementToHandler: (decrementTo: number) => (textEditor: TextEditor) => void = (decrementTo) => async (textEditor) => {
+  if (textEditor.selections.length === 0) {
+    return;
+  }
+  textEditor.edit((editBuilder) => {
+    const start = textEditor.selections.length + decrementTo - 1;
+    textEditor.selections
+      .forEach((selection, index) => {
+        editBuilder.replace(selection, (start - index).toString());
+      });
+  });
+};
+
+export const decrementToInputHandler: (textEditor: TextEditor) => void = async (textEditor) => {
+  if (textEditor.selections.length === 0) {
+    return;
+  }
+  let decrementTo = Number(await window.showInputBox({
+    prompt: 'Decrement To',
+    ignoreFocusOut: true,
+  }));
+  decrementToHandler(decrementTo)(textEditor);
+};
