ccf72e21cf297bae9466151ff08df8ee337a8135
Takashi Imahiro <imahiro@erin.jp>
2023-12-22 22:17:48 +0900

    ✨ add tools for increment by and decrement by

---
+++ src/handler/decrementHandler.ts
@@ -0,0 +1,34 @@
+import {
+  TextEditor,
+  window,
+} from 'vscode';
+
+export const decrementByHandler: (decrementBy: number) => (textEditor: TextEditor) => void = (decrementBy) => async (textEditor) => {
+  if (textEditor.selections.length === 0) {
+    return;
+  }
+  textEditor.edit((editBuilder) => {
+    const array = textEditor.selections
+      .forEach(selection => {
+        const text = textEditor.document.getText(selection);
+        editBuilder.replace(selection, decrement(decrementBy)(text));
+      });
+  });
+};
+
+export const decrementHandler: (textEditor: TextEditor) => void = async (textEditor) => {
+  if (textEditor.selections.length === 0) {
+    return;
+  }
+  let decrementBy = Number(await window.showInputBox({
+    prompt: 'Increment by',
+    placeHolder: `Default: 1`,
+    ignoreFocusOut: true,
+  }));
+  decrementBy = decrementBy > 0 ? decrementBy : 1;
+  decrementByHandler(decrementBy)(textEditor);
+};
+
+const decrement: (num: number) => (value: string) => string = (num) => (value) => {
+  return isNaN(Number(value)) ? value : (Number(value) - num).toString();
+};
