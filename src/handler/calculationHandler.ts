7251a21d40cadb5dae81ae2124448d4c906086b7
Takashi Imahiro <imahiro@erin.jp>
2023-12-24 19:37:34 +0900

    💄 change calculation output format

--- src/handler/calculationHandler.ts
+++ src/handler/calculationHandler.ts
@@ -1,26 +1,25 @@
 import {
   TextEditor,
 } from 'vscode';
-import { openTextDocument } from '../common';
+import { openTextDocumentWithTitles } from '../common';
 
 export const calculationHandler: (textEditor: TextEditor) => void = (textEditor) => {
   if (textEditor.selections.length === 0) {
     return;
   }
-  const content =
+  const zip =
     textEditor.selections
-      .map(selection => textEditor.document.getText(selection))
+      .map(selection => textEditor.document.getText(selection).trim())
       .join("\n")
       .split("\n")
-      .filter(x => x.trim() !== '')
-      .map(expression => `${expression.trim()} = ${calc(expression)}`)
-      .join("\n");
-  openTextDocument(content);
+      .filter(x => x !== '')
+      .map(expression => [expression, calc(expression)]);
+  openTextDocumentWithTitles(zip);
 };
 
 const calc: (expression: string) => string = (expression) => {
   try {
-    return eval(expression);
+    return eval(expression).toString();
   } catch (_e) {
     return 'NaN';
   }
