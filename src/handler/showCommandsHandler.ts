b6f2d247f8c63dc52bb4364d746fe525e93bd5ee
Takashi Imahiro <imahiro@erin.jp>
2023-12-26 08:58:57 +0900

    💄 Commands that cannot be used in multiple selection mode are now hidden

--- src/handler/showCommandsHandler.ts
+++ src/handler/showCommandsHandler.ts
@@ -7,251 +7,313 @@ import {
 const myCommands = [
   {
     "command": "selection-manipulator.multi-selection",
-    "title": "Select - Convert to Multi Selection"
+    "title": "Select - Convert to Multi Selection",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.extract",
-    "title": "Extract - Extract Lines"
+    "title": "Extract - Extract Lines",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.extract.exclude-blank-rows",
-    "title": "Extract - Extract Lines exclude Blank Rows"
+    "title": "Extract - Extract Lines exclude Blank Rows",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.unique",
-    "title": "Extract - Unique Lines"
+    "title": "Extract - Unique Lines",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.reverse",
-    "title": "Extract - Reverse Lines"
+    "title": "Extract - Reverse Lines",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.shuffle",
-    "title": "Extract - Shuffle Lines"
+    "title": "Extract - Shuffle Lines",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.sort.string.ascending",
-    "title": "Extract - Sort Lines Ascending by string"
+    "title": "Extract - Sort Lines Ascending by string",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.sort.string.descending",
-    "title": "Extract - Sort Lines Descending by string"
+    "title": "Extract - Sort Lines Descending by string",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.sort.number.ascending",
-    "title": "Extract - Sort Lines Ascending by number"
+    "title": "Extract - Sort Lines Ascending by number",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.sort.number.descending",
-    "title": "Extract - Sort Lines Descending by number"
+    "title": "Extract - Sort Lines Descending by number",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.json.format",
-    "title": "Transform - JSON - Format JSON (Pretty Print)"
+    "title": "Transform - JSON - Format JSON (Pretty Print)",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.json.minify",
-    "title": "Transform - JSON - Minify JSON"
+    "title": "Transform - JSON - Minify JSON",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.json.parse",
-    "title": "Transform - JSON - Parse JSON"
+    "title": "Transform - JSON - Parse JSON",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.json.stringify",
-    "title": "Transform - JSON - Stringify JSON"
+    "title": "Transform - JSON - Stringify JSON",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.xml.format",
-    "title": "Transform - XML - Format XML (Pretty Print)"
+    "title": "Transform - XML - Format XML (Pretty Print)",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.xml.minify",
-    "title": "Transform - XML - Minify XML"
+    "title": "Transform - XML - Minify XML",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.base64.encode",
-    "title": "Transform - Base64 - Encode Base64"
+    "title": "Transform - Base64 - Encode Base64",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.base64.decode",
-    "title": "Transform - Base64 - Decode Base64"
+    "title": "Transform - Base64 - Decode Base64",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.base64.deflate",
-    "title": "Transform - Base64 - Deflate Base64"
+    "title": "Transform - Base64 - Deflate Base64",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.base64.unzip",
-    "title": "Transform - Base64 - Inflate Base64"
+    "title": "Transform - Base64 - Inflate Base64",
+    "canMultiSelection": false
   },
   {
     "command": "selection-manipulator.dns.a",
-    "title": "Transform - DNS - A Record"
+    "title": "Transform - DNS - A Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.aaaa",
-    "title": "Transform - DNS - AAAA Record"
+    "title": "Transform - DNS - AAAA Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.any",
-    "title": "Transform - DNS - ANY Record"
+    "title": "Transform - DNS - ANY Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.caa",
-    "title": "Transform - DNS - CAA Record"
+    "title": "Transform - DNS - CAA Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.cname",
-    "title": "Transform - DNS - CNAME Record"
+    "title": "Transform - DNS - CNAME Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.mx",
-    "title": "Transform - DNS - MX Record"
+    "title": "Transform - DNS - MX Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.naptr",
-    "title": "Transform - DNS - NAPTR Record"
+    "title": "Transform - DNS - NAPTR Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.ns",
-    "title": "Transform - DNS - NS Record"
+    "title": "Transform - DNS - NS Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.ptr",
-    "title": "Transform - DNS - PTR Record"
+    "title": "Transform - DNS - PTR Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.soa",
-    "title": "Transform - DNS - SOA Record"
+    "title": "Transform - DNS - SOA Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.srv",
-    "title": "Transform - DNS - SRV Record"
+    "title": "Transform - DNS - SRV Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.txt",
-    "title": "Transform - DNS - TXT Record"
+    "title": "Transform - DNS - TXT Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.dns.reverse",
-    "title": "Transform - DNS - REVERSE Record"
+    "title": "Transform - DNS - REVERSE Record",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.count-occurrences.count",
-    "title": "Transform - Count Occurrences sorting by count"
+    "title": "Transform - Count Occurrences sorting by count",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.count-occurrences.word",
-    "title": "Transform - Count Occurrences sorting by word"
+    "title": "Transform - Count Occurrences sorting by word",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.count-up-list",
-    "title": "Transform - Count Up to List"
+    "title": "Transform - Count Up to List",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.camel",
-    "title": "Replace - Case - Change Case Camel"
+    "title": "Replace - Case - Change Case Camel",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.capital",
-    "title": "Replace - Case - Change Case Capital"
+    "title": "Replace - Case - Change Case Capital",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.constant",
-    "title": "Replace - Case - Change Case Constant"
+    "title": "Replace - Case - Change Case Constant",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.dot",
-    "title": "Replace - Case - Change Case Dot"
+    "title": "Replace - Case - Change Case Dot",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.kebab",
-    "title": "Replace - Case - Change Case Kebab"
+    "title": "Replace - Case - Change Case Kebab",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.no",
-    "title": "Replace - Case - Change Case No"
+    "title": "Replace - Case - Change Case No",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.pascal",
-    "title": "Replace - Case - Change Case Pascal"
+    "title": "Replace - Case - Change Case Pascal",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.path",
-    "title": "Replace - Case - Change Case Path"
+    "title": "Replace - Case - Change Case Path",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.sentence",
-    "title": "Replace - Case - Change Case Sentence"
+    "title": "Replace - Case - Change Case Sentence",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.snake",
-    "title": "Replace - Case - Change Case Snake"
+    "title": "Replace - Case - Change Case Snake",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.train",
-    "title": "Replace - Case - Change Case Train"
+    "title": "Replace - Case - Change Case Train",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.upper",
-    "title": "Replace - Case - Change Case Upper"
+    "title": "Replace - Case - Change Case Upper",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.case.lower",
-    "title": "Replace - Case - Change Case Lower"
+    "title": "Replace - Case - Change Case Lower",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.zero-padding",
-    "title": "Replace - Number - Zero Padding"
+    "title": "Replace - Number - Zero Padding",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.increment-from-1",
-    "title": "Replace - Number - Increment from 1"
+    "title": "Replace - Number - Increment from 1",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.increment-from-n",
-    "title": "Replace - Number - Increment from N"
+    "title": "Replace - Number - Increment from N",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.decrement-to-1",
-    "title": "Replace - Number - Decrement to 1"
+    "title": "Replace - Number - Decrement to 1",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.decrement-to-n",
-    "title": "Replace - Number - Decrement to N"
+    "title": "Replace - Number - Decrement to N",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.increment-by-1",
-    "title": "Replace - Number - Increment by 1"
+    "title": "Replace - Number - Increment by 1",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.increment-by-n",
-    "title": "Replace - Number - Increment by N"
+    "title": "Replace - Number - Increment by N",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.decrement-by-1",
-    "title": "Replace - Number - Decrement by 1"
+    "title": "Replace - Number - Decrement by 1",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.decrement-by-n",
-    "title": "Replace - Number - Decrement by N"
+    "title": "Replace - Number - Decrement by N",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.calculation",
-    "title": "Calculate - Calculate Mathematical Expression"
+    "title": "Calculate - Calculate Mathematical Expression",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.calculation.date",
-    "title": "Calculate - Transform to Timestamp and ISO 8601"
+    "title": "Calculate - Transform to Timestamp and ISO 8601",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.regex.g",
-    "title": "Regular Expression - Regex (/PATTERN/g)"
+    "title": "Regular Expression - Regex (/PATTERN/g)",
+    "canMultiSelection": true
   },
   {
     "command": "selection-manipulator.regex.gi",
-    "title": "Regular Expression - Regex (/PATTERN/gi)"
+    "title": "Regular Expression - Regex (/PATTERN/gi)",
+    "canMultiSelection": true
   }
 ];
 
@@ -259,8 +321,11 @@ export const showCommandsHandler: (textEditor: TextEditor) => void = async (text
   if (textEditor.selections.length === 0) {
     return;
   }
+  const isMultiSelection = textEditor.selections.length > 1;
   const options = { title: "Selection Manipulator Commands", canPickMany: false };
-  const commandTitles = myCommands.map(command => command.title);
+  const commandTitles = myCommands
+    .filter(command => isMultiSelection ? command.canMultiSelection : true)
+    .map(command => command.title);
   const selectedCommandTitle = await window.showQuickPick(commandTitles, options);
   if (!selectedCommandTitle) {
     return;
