f02cca8907d0f455eb665e50dc4106fa7a3f0ef9
Takashi Imahiro <imahiro@erin.jp>
2023-12-26 08:57:03 +0900

    💄 In base64, only single selection is allowed

--- src/handler/base64Handler.ts
+++ src/handler/base64Handler.ts
@@ -6,23 +6,20 @@ import { openTextDocument } from '../common';
 type command = 'encode' | 'decode' | 'deflate' | 'inflate';
 
 export const base64Handler: (command: command) => (textEditor: TextEditor) => void = (command) => async (textEditor) => {
-  if (textEditor.selections.length === 0) {
+  const selectedText = textEditor.document.getText(textEditor.selection);
+  if (!selectedText) {
     return;
   }
   const zlib = require('node:zlib');
-  const text =
-    textEditor.selections
-      .map(selection => textEditor.document.getText(selection))
-      .join("\n");
   let content;
   if (command === 'encode') {
-    content = btoa(text);
+    content = btoa(selectedText);
   } else if (command === 'decode') {
-    content = atob(text);
+    content = atob(selectedText);
   } else if (command === 'deflate') {
-    content = zlib.deflateRawSync(Buffer.from(text)).toString('base64');
+    content = zlib.deflateRawSync(Buffer.from(selectedText)).toString('base64');
   } else if (command === 'inflate') {
-    content = zlib.inflateRawSync(Buffer.from(text, 'base64')).toString();
+    content = zlib.inflateRawSync(Buffer.from(selectedText, 'base64')).toString();
   }
   openTextDocument(content);
 };
